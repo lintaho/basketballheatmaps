91318b3159df454207aa126fbc30960b046dbf58
lintaho <linhao.zhang@gmail.com>
2013-11-24 18:39:05 -0800

    cleanup

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,152 +1,29 @@
 $(window).ready(function () {
 
-$("#slider").dateRangeSlider({
+	$("#slider").dateRangeSlider({
 
-	arrows:false,
-	bounds:{
-		min: new Date(2012, 10, 30), 
-		max: new Date(2013, 6, 20)
-	},
-	defaultValues:{
-		min: new Date(2012, 10, 30),
-		max: new Date(2013, 4, 17)
-	}
-
-});
-
-$("#slider").on("valuesChanging", function(e, data){
-	$.ajax({
-		url: "/shots",
-		type: "POST",
-		dataType: "json",
-		data: {
-			name: $('#inputPlayer').val(),
-			made: $("#madebox").prop('checked'),
-			missed: $("#missedbox").prop('checked'),
-			qtr: [$("#onebox").prop('checked'), $("#twobox").prop('checked'), $("#threebox").prop('checked'),
-				$("#fourbox").prop('checked'), $("#OTbox").prop('checked')],
-			startdate: $("#slider").dateRangeSlider("values").min.toISOString(),
-			enddate: $("#slider").dateRangeSlider("values").max.toISOString()
+		arrows:false,
+		bounds:{
+			min: new Date(2012, 10, 30), 
+			max: new Date(2013, 6, 20)
 		},
-	    success: function(data) {
-	      showData(data);
-	    },
-	    error: function(e) {
-	        console.log("error");
-	    }
+		defaultValues:{
+			min: new Date(2012, 10, 30),
+			max: new Date(2013, 4, 17)
+		}
+
 	});
-});
 
-var config = {
-	element: document.getElementById("heat"),
-	radius: 20,
-	opacity: 70
-};
+	var config = {
+		element: document.getElementById("heat"),
+		radius: 20,
+		opacity: 70
+	};
 
-var heatmap = h337.create(config);
+	var heatmap = h337.create(config);
 
-function showData(data){
-	coords = [];
-	var shots = "";
-	for(var k in data){
-		coords.push({x:data[k]['x']*6, y:data[k]['y']*6, count:1})
-		shots += data[k]['x'] + "<br>";
-	}
-	var heatpoints = {
-		max: 3,
-		data: coords		
-	};	
-	heatmap.store.setDataSet(heatpoints);
-}
-
-
-$('#inputPlayer').autocomplete({
-	source: "/players",
-	minLength: 2,
-	select: function(event, ui){
-
-
+	function ajaxReq(){
 		$.ajax({
-			url: "/shots",
-			type: "POST",
-			dataType: "json",
-			data: {
-				name: ui['item']['value'],
-				made: $("#madebox").prop('checked'),
-				missed: $("#missedbox").prop('checked'),
-				qtr: [$("#onebox").prop('checked'), $("#twobox").prop('checked'), $("#threebox").prop('checked'),
-				$("#fourbox").prop('checked'), $("#OTbox").prop('checked')],
-				startdate: $("#slider").dateRangeSlider("values").min.toISOString(),
-				enddate: $("#slider").dateRangeSlider("values").max.toISOString()
-			},
-		    success: function(data) {
-		      showData(data);
-		    },
-		    error: function(e) {
-		        console.log(e);
-		    }
-		});
-
-
-	}, 
-	 messages: {
-        noResults: '',
-        results: function() {}
-    }
-
-});
-
-
-$('#madebox').change(function() {
-    $.ajax({
-		url: "/shots",
-		type: "POST",
-		dataType: "json",
-		data: {
-			name: $('#inputPlayer').val(),
-			made: $("#madebox").prop('checked'),
-			missed: $("#missedbox").prop('checked'),
-			qtr: [$("#onebox").prop('checked'), $("#twobox").prop('checked'), $("#threebox").prop('checked'),
-				$("#fourbox").prop('checked'), $("#OTbox").prop('checked')],
-			startdate: $("#slider").dateRangeSlider("values").min.toISOString(),
-			enddate: $("#slider").dateRangeSlider("values").max.toISOString()
-		},
-	    success: function(data) {
-	      showData(data);
-	    },
-	    error: function(e) {
-	        console.log("error");
-	    }
-	});
-}); 
-
-$('#missedbox').change(function() {
-    $.ajax({
-		url: "/shots",
-		type: "POST",
-		dataType: "json",
-		data: {
-			name: $('#inputPlayer').val(),
-			made: $("#madebox").prop('checked'),
-			missed: $("#missedbox").prop('checked'),
-			qtr: [$("#onebox").prop('checked'), $("#twobox").prop('checked'), $("#threebox").prop('checked'),
-				$("#fourbox").prop('checked'), $("#OTbox").prop('checked')],
-			startdate: $("#slider").dateRangeSlider("values").min.toISOString(),
-			enddate: $("#slider").dateRangeSlider("values").max.toISOString()
-		},
-	    success: function(data) {
-	      showData(data);
-	    },
-	    error: function(e) {
-	        console.log("error");
-	    }
-	});
-}); 
-
-
-$("#qtrs_boxes :checkbox").change(function(e){
-
-	$.ajax({
 			url: "/shots",
 			type: "POST",
 			dataType: "json",
@@ -166,10 +43,68 @@ $("#qtrs_boxes :checkbox").change(function(e){
 		        console.log("error");
 		    }
 		});
+	}
 
-});
+	function showData(data){
+		coords = [];
+		var shots = "";
+		for(var k in data){
+			coords.push({x:data[k]['x']*6, y:data[k]['y']*6, count:1})
+			shots += data[k]['x'] + "<br>";
+		}
+		var heatpoints = {
+			max: 3,
+			data: coords		
+		};	
+		heatmap.store.setDataSet(heatpoints);
+	}
 
 
+	$("#slider").on("valuesChanged", function(e, res){
+		ajaxReq();
+	});
 
+	$('#madebox').change(function() {
+	    ajaxReq();
+	}); 
+
+	$('#missedbox').change(function() {
+	    ajaxReq();
+	}); 
+
+	$("#qtrs_boxes :checkbox").change(function(e){
+		ajaxReq();
+	});
+
+	$('#inputPlayer').autocomplete({
+		source: "/players",
+		minLength: 2,
+		select: function(event, ui){
+			$.ajax({
+				url: "/shots",
+				type: "POST",
+				dataType: "json",
+				data: {
+					name: ui['item']['value'],
+					made: $("#madebox").prop('checked'),
+					missed: $("#missedbox").prop('checked'),
+					qtr: [$("#onebox").prop('checked'), $("#twobox").prop('checked'), $("#threebox").prop('checked'),
+					$("#fourbox").prop('checked'), $("#OTbox").prop('checked')],
+					startdate: $("#slider").dateRangeSlider("values").min.toISOString(),
+					enddate: $("#slider").dateRangeSlider("values").max.toISOString()
+				},
+			    success: function(data) {
+			      showData(data);
+			    },
+			    error: function(e) {
+			        console.log(e);
+			    }
+			});
+		}, 
+		 messages: {
+	        noResults: '',
+	        results: function() {}
+	    }
+	});
 
 });
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@
  */
 
 exports.index = function(db){
-
 	return function(req, res){
-		console.log(req)	
 		res.render('index', { title: 'NBA Heatmaps' });
 	};
 };
@@ -14,24 +12,11 @@ exports.index = function(db){
 exports.players = function(db){
 	return function(req, res){
 		var collection = db.get('player_names');
-		// console.log(req.body.startdate)
 		var name = req.query.term
 		var re = new RegExp(".*"+name+".*", "i")
 		collection.find({"p":re}, ["p"], function(e, docs){
 			res.json(docs.map(function(item){return item.p}));
-			// console.log(e)
-			res.render('index',  {title: "tETST"})
 		});
-		// date_map.find({"date": {$gte: new Date(req.body.startdate), $lt: new Date(req.body.enddate)}},
-		// 			{"gameId":1}, 
-		// 			function(e, docs){
-		// 		collection.find({"p": name, "gameId": {$in: docs.map(function(game) {return game.gameId;})}}, function(e, docs){
-		// 		res.contentType('json');	
-		// 		res.json(docs);	
-		// 	});
-
-		// });
-
 	};
 }
 
@@ -43,8 +28,8 @@ exports.shots = function(db){
 		
 		var name = req.body.name
 
-		var made = req.body.made  //true, show made shots
-		var missed = req.body.missed //true show missed shots
+		var made = req.body.made  //true => show made shots
+		var missed = req.body.missed //true => show missed shots
 
 		var show_shots = []
 		if (made === "true" && missed ==="true"){
